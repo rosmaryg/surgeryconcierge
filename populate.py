5a1f44716cbde07e2d7a14ecb7b5c8badeb2d2a9
Rosmary George <rosmaryg@seas.upenn.edu>
2015-11-23 18:05:49 -0500

    changed arguments for get_pdf()

--- populate.py
+++ populate.py
@@ -4,8 +4,7 @@ import fpdf
 
 
 # generate a pdf from the json template that provides instructions
-def gen_pdf(json_template):
-	data = json.loads(json_template)
+def gen_pdf(surgery_name, month, day, year, insns):
 	pdf = fpdf.FPDF(format='letter')
 	pdf.set_auto_page_break(True,margin=0)
 	pdf.add_page()
@@ -13,7 +12,8 @@ def gen_pdf(json_template):
 	pdf.cell(195, 10, txt="Pre-Surgery Instructions", align="C")
 	pdf.ln(h='')
 	pdf.set_font("Arial", size=10)
-	intro = "Your surgery is scheduled on " + data[1]["surgery_date"]["printed"] + ". You will receive \
+	surg_date = datetime.date(year, month, day).strftime("%A, %B %d, %Y")
+	intro = "Your surgery is scheduled on " + surg_date + ". You will receive \
 	a phone call to tell you what time to arrive at the hospital. If you do not receive a call by noon \
 	the day before your surgery, please call the office. If you can't keep your surgery appointment, \
 	call your surgeon's office."
@@ -21,7 +21,7 @@ def gen_pdf(json_template):
  	pdf.line(pdf.get_x(), pdf.get_y(), pdf.get_x() + 195, pdf.get_y())
 	pdf.ln(h='')
 
- 	insns = data[2]["insns"]
+ 	insns = insns
  	for num in range(len(insns)):
  		insn = insns[num]
 		pdf.set_font("Arial", style="B", size=10)
@@ -37,14 +37,13 @@ def gen_pdf(json_template):
 # function takes in the date of the surgery as three parameters (month, day, year)
 def populate(month, day, year, results, surgery_name):
 
-	update = []
-	update.append({"surgery_name": surgery_name})
+	update = {}
+	update["surgery_name"] = surgery_name
 	surg_date = {}
 	surg_date["month"] = month
 	surg_date["day"] = day
 	surg_date["year"] = year
-	surg_date["printed"] = datetime.date(year, month, day).strftime("%A, %B %d, %Y")
-	update.append({"surgery_date": surg_date})
+	update["surgery_date"] = surg_date
 	insns = []
 	for result in results:
 		current = {}
@@ -54,7 +53,7 @@ def populate(month, day, year, results, surgery_name):
 		current["ask_doctor"] = result[2]
 		current["insn_text"] = result[3]
 		insns.append(current)
-	update.append({"insns": insns})
+	update["insns"] = insns
 
 	# jsonFile = open("populated.json", "w+")
 	# jsonFile.write(json.dumps(update))
@@ -87,5 +86,28 @@ results = [
 	]
 ]
 
-#blah = populate(11, 17, 2015, results, "blah")
-#gen_pdf(blah)
+insns = [
+    {
+      "date": "Tuesday, November 03, 2015",
+      "conditions": "",
+      "ask_doctor": 0,
+      "insn_text": "Stop taking these herbal products, nutritional supplements: Echinacea, Ephedra, Feverfew, Garlic, Ginger, Ginkgo biloba, Ginseng, Kava Kava, Saw palmetto, St. John''s West, Fish oil, Vitamin B"
+    },
+    {
+      "date": "Tuesday, November 10, 2015",
+      "conditions": "",
+      "ask_doctor": 0,
+      "insn_text": "Stop taking all medicines containing aspirin: Aspirin, Anacin, Ascriptin, Pepto-Bismol, Bufferin, Alka-Seltzer, Excedrin, Florinal, Lortab ASA"
+    },
+    {
+      "date": "Tuesday, November 17, 2015",
+      "conditions": "",
+      "ask_doctor": 0,
+      "insn_text": "An adult will need to pick you up after surgery"
+    }
+  ]
+
+
+blah = populate(11, 17, 2015, results, "blah")
+print blah
+gen_pdf(blah, 11, 17, 2015, insns)
