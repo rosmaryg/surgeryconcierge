75373aa93cc02802b7cfe61a8c57c30a1cc2ef97
Joyce Lee <joyce@SEAS173.wlan.seas.upenn.edu>
2016-02-11 13:47:53 -0500

    fixed button text

--- pilot/server.py
+++ pilot/server.py
@@ -16,7 +16,7 @@ def calendar():
 def genCalendar():
   ret_val = subprocess.call(["python", "new_calendar.py", "--data", "\"" + str(request.args) + "\""])
   if ret_val == 0:
-    return 'Calendar succesfully created!   <a href="http://google.com/calendar">View my calendar</a> <br><br> <button onClick=\"window.close()\"">Close this window and get PDF</button>'
+    return 'Calendar succesfully created!   <a href="http://google.com/calendar">View my calendar</a> <br><br> <button onClick=\"window.close()\"">Close window</button>'
   else:
     return 'Calendar not created.'
 
