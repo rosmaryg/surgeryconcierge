b7a5c4a41b3478595e68172b2d5cc6a477527dac
Rosmary George <rosmaryg@seas.upenn.edu>
2016-02-11 02:55:49 -0500

    painful pdfs

---
+++ pilot/new_pdf.py
@@ -0,0 +1,181 @@
+from __future__ import print_function
+import httplib2
+import os
+import sys
+from collections import defaultdict
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+import datetime
+import json
+import easygui
+import fpdf
+
+try:
+    import argparse
+    parser = argparse.ArgumentParser(parents=[tools.argparser])
+    parser.add_argument('--data', help="User data from html form")
+    flags = parser.parse_args()
+except ImportError:
+    flags = None
+
+insn_table = {
+'0' : "0: Be prepared to spend a full day at the hospital. Wear loose, comfortable clothing and do not bring valuables. Bring a list of all medications you take.",
+'1' : "0: Do not wear jewelry, wedding bands, body piercings, makeup, nail polish, artificial nails",
+'2' : "0: Do not wear contact lenses",
+'3' : "0: Do not eat or drink anything",
+'4' : "1: Do not eat or drink anything after midnight (no water, coffee, gum, lifesavers, ice, food, etc.)",
+'5' : "0: Take all meds with a sip of water at your usual times (except diabetes meds)",
+'6' : "0: Bring the following to the hospital - inhalers, CPAP mask, eye drops",
+'7' : "1: Do not drink any alcoholic beverages or smoke 24 hours prior to surgery",
+'8' : "14: Stop taking these herbal products, nutritional supplements - ",
+'8a' : "Echinacea", 
+'8b' : "Ephedra", 
+'8c' : "Feverfew", 
+'8d' : "Garlic", 
+'8e' : "Ginger", 
+'8f' : "Ginkgo biloba", 
+'8g' : "Ginseng", 
+'8h' : "Kava Kava", 
+'8i' : "Saw palmetto", 
+'8j' : "St. John''s West", 
+'8k' : "Fish oil", 
+'8l' : "Vitamin B",
+'9' : "7: Stop taking all medicines containing aspirin - ", 
+'9a' : "Aspirin", 
+'9b' : "Anacin", 
+'9c' : "Ascriptin", 
+'9d' : "Pepto-Bismol", 
+'9e' : "Bufferin", 
+'9f' : "Alka-Seltzer", 
+'9g' : "Excedrin", 
+'9h' : "Florinal", 
+'9i' : "Lortab ASA",
+'10': "7: Stop taking Non-Steroidal Anti-inflammatory Drugs (NSAIDs) - ",
+'10a' : "Ibuprofen (Advil Motrin, Nuprin, Medipren)", 
+'10b' : "Naproxen (Aleve, Anaprox, Naprosyn)", 
+'10c' : "Diclofenac (Cataflam, Voltaren, Arthrotec)", 
+'10d' : "Celebrex", 
+'10e' : "Toradol (ketorolac)", 
+'10f' : "Lodine (etodolac)", 
+'10g' : "Feldene (piroxicam)", 
+'10h' : "Relefeo (nuburnetce)",
+'11': "7: Stop taking Accutane",
+'12': "3: Stop taking Suboxone",
+'13': "3: Cialis, Viagra, and Levitra",
+'14': "5:Stop blood thinner warfarin (Coumadin); switch to Lovenox (enoxaparin) temporarily if advised by doctor",
+'15': "7: Stop the following blood thinning medicines - ",
+'15a' : "Plavix (Clopidogrel)", 
+'15b' : "Xarelto (Rivaroxaban)", 
+'15c' : "Ticlid (Ticlopidine)", 
+'15d' : "Pletal (Cilostazol)", 
+'15e' : "Brilinta (Ticagrelor)", 
+'15f' : "Effient (prasugrel)", 
+'15g' : "Aggrenox (Aspirin-Dipridarrola)",
+'16': "1: Follow instructions from doctor for diabetes",
+'17': "0: Do not take any medicine that is a water pill (diuretic) including - ",
+'17a' : "furosemide (Lasix)", 
+'17b' : "hydrochlorothiazide (HCTZ)", 
+'17c' : "medicines combined with hydrochlorothiazide (HCT)"
+}
+
+def get_user_input(data):
+    entries = data.split('[')[1].split(']')[0].split('),')
+    d = defaultdict(str)
+    for entry in entries:
+        key = entry.split(',')[0].split('\'')[1]
+        val = entry.split(',')[1].split('\'')[1]
+        d[key] = val
+    return d
+
+# generate a pdf from the json template that provides instructions
+def gen_pdf(surg_info, insns_json):
+    month = surg_info[0]
+    day = surg_info[1]
+    year = surg_info[2]
+    pdf = fpdf.FPDF(format='letter')
+    pdf.set_auto_page_break(True,margin=0)
+    pdf.add_page()
+    pdf.set_font("Arial", style="B", size=12)
+    pdf.cell(195, 10, txt="Pre-Surgery Instructions", align="C")
+    pdf.ln(h='')
+    pdf.set_font("Arial", size=10)
+    surg_date = datetime.date(year, month, day).strftime("%A, %B %d, %Y")
+    intro = "Your surgery is scheduled on " + surg_date + ". You will receive a phone call to tell you what time to arrive at the hospital. If you do not receive a call by noon the day before your surgery, please call the office. If you can't keep your surgery appointment, call your surgeon's office."
+    pdf.multi_cell(195, 10, txt=intro, align="L")
+    pdf.line(pdf.get_x(), pdf.get_y(), pdf.get_x() + 195, pdf.get_y())
+    pdf.ln(h='')
+
+    insns = json.loads(insns_json)
+    for num in range(len(insns)):
+        insn = insns[num]
+        pdf.set_font("Arial", style="B", size=10)
+        pdf.multi_cell(195, 10, txt=insn["date"], align="L")
+        pdf.set_font("Arial", size=10)
+        pdf.multi_cell(195, 7, txt=insn["insn_text"], align="L")
+        pdf.set_font("Arial", size=2)
+        pdf.multi_cell(195, 5, txt="\n", align="L")
+
+    # pdf.close()
+    pdf_string = pdf.output(name="Instructions.pdf", dest="S")
+    return pdf_string
+
+def generate_pdf():
+
+    input = get_user_input(flags.data)
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+
+    year = input['year']
+    if input['month'] == "":
+        month = easygui.enterbox(
+            msg="Month of procedure:",
+            title="Surgery Concierge",
+            strip=True,
+            default="MM")
+    else:
+        month = input['month']
+
+    if input['day'] == "":
+        day = easygui.enterbox(
+            msg="Day of procedure",
+            title="Surgery Concierge",
+            strip=True,
+            default="DD")
+    else:
+        day = input['day']
+    cat_insns = {}
+    insns_for_pdf = []
+    #Create a dict for the beginning of multi-part insns
+    for insn in input:
+        if 'insn' in insn and not insn[-1].isalpha():
+            cat_insns[insn] = insn_table[input[insn]]
+    for insn in input:
+        if 'insn' in insn and insn[-1].isalpha(): 
+            base_insn = insn[:-1]
+            current_str = cat_insns[base_insn]
+            if current_str[-1] != ' ':
+                    cat_insns[base_insn] = current_str + ', ' + insn_table[input[insn]] 
+            else:
+                    cat_insns[base_insn] = current_str + insn_table[input[insn]]
+    
+    for insn in input:
+        if 'insn' not in insn or insn[-1].isalpha():
+            continue
+        i = insn_table[input[insn]]
+
+        insn_for_pdf = {}
+        date = datetime.date(int(year), int(month), int(day)) - datetime.timedelta(int(i.split(':')[0]))
+        insn_for_pdf["date"] = date.strftime("%A, %B %d, %Y")
+        insn_for_pdf["insn_text"] = cat_insns[insn].split(':')[1]
+        insns_for_pdf.append(insn_for_pdf)
+
+    surg_info = [int(month), int(day), int(year)]
+    pdf =  gen_pdf(surg_info, json.dumps(insns_for_pdf))
+    print (pdf)
+    return pdf
+
+if __name__ == '__main__':
+    generate_pdf()
+
--- pilot/server.py
+++ pilot/server.py
@@ -1,6 +1,6 @@
 import subprocess
 from flask import request
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
@@ -16,9 +16,25 @@ def calendar():
 def genCalendar():
   ret_val = subprocess.call(["python", "new_calendar.py", "--data", "\"" + str(request.args) + "\""])
   if ret_val == 0:
-    return 'Calendar succesfully created! You may now close this window. <br> <a href="http://google.com/calendar">View my calendar<a>'
+    return 'Calendar succesfully created!   <a href="http://google.com/calendar">View my calendar</a> <br><br> <button onClick=\"window.close()\"">Close this window and get PDF</button>'
   else:
     return 'Calendar not created.'
+
+@app.route('/generate-pdf')
+def genPdf():
+  pdf = subprocess.check_output(["python", "new_pdf.py", "--data", "\"" + str(request.args) + "\""])
+  print "before pdf"
+  print "pdf " + pdf
+  print "after pdf"
+  response = make_response(pdf)
+  response.headers['Content-Disposition'] = "inline; filename='Instructions.pdf"
+  response.mimetype = 'application/pdf'
+  return response
+  # ret_val = subprocess.call(["python", "new_calendar.py", "--data", "\"" + str(request.args) + "\""])
+  # if ret_val == 0:
+  #   return 'Calendar succesfully created! You may now close this window. <br> <a href="http://google.com/calendar">View my calendar<a>'
+  # else:
+  #   return 'Calendar not created.'
 
 if __name__ == '__main__':
   app.run(debug=True)
