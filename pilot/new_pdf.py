9d97a833dd176df714aa0b6266e236ee82e2dd07
Rosmary George <rosmaryg@seas.upenn.edu>
2016-02-21 22:52:21 -0500

    pdf updates

--- pilot/new_pdf.py
+++ pilot/new_pdf.py
@@ -93,7 +93,7 @@ def get_user_input(data):
     return d
 
 # generate a pdf from the json template that provides instructions
-def gen_pdf(surg_info, insns_json):
+def gen_pdf(surg_info, insns):
     month = surg_info[0]
     day = surg_info[1]
     year = surg_info[2]
@@ -110,14 +110,28 @@ def gen_pdf(surg_info, insns_json):
     pdf.line(pdf.get_x(), pdf.get_y(), pdf.get_x() + 195, pdf.get_y())
     pdf.ln(h='')
 
-    insns = json.loads(insns_json)
-    for num in range(len(insns)):
-        insn = insns[num]
+    keys = insns.keys()
+    int_keys = []
+    for key in keys:
+        int_keys.append(int(key))
+    sorted_keys = int_keys.sort(reverse=True)
+    sorted_str_keys =[]
+    for key in int_keys:
+        sorted_str_keys.append(str(key))
+
+    # insns = json.loads(insns_json)
+    for key in sorted_str_keys:
+        insns_for_day = insns[key]
+                # date = datetime.date(int(year), int(month), int(day)) - datetime.timedelta(int(i.split(':')[0]))
+        # insn_for_pdf["num_days"] = int(i.split(':')[0])
+        insn_date = (datetime.date(year, month, day) - datetime.timedelta(int(key))).strftime("%A, %B %d, %Y")
         pdf.set_font("Arial", style="B", size=10)
-        pdf.multi_cell(195, 10, txt=insn["date"], align="L")
-        pdf.set_font("Arial", size=10)
-        pdf.multi_cell(195, 7, txt=insn["insn_text"], align="L")
-        pdf.set_font("Arial", size=2)
+        pdf.multi_cell(195, 10, txt=insn_date, align="L")
+        for indiv_insn in insns_for_day:
+            pdf.set_font("Arial", size=10)
+            pdf.multi_cell(195, 7, txt=indiv_insn, align="L")
+            pdf.set_font("Arial", size=2)
+            pdf.multi_cell(195, 5, txt="\n", align="L")
         pdf.multi_cell(195, 5, txt="\n", align="L")
 
     # pdf.close()
@@ -154,7 +168,7 @@ def generate_pdf():
     day = input['day']
     
     cat_insns = {}
-    insns_for_pdf = []
+    insns_for_pdf = {}
     #Create a dict for the beginning of multi-part insns
     for insn in input:
         if 'insn' in insn and not insn[-1].isalpha():
@@ -173,16 +187,27 @@ def generate_pdf():
             continue
         i = insn_table[input[insn]]
 
-        insn_for_pdf = {}
-        date = datetime.date(int(year), int(month), int(day)) - datetime.timedelta(int(i.split(':')[0]))
-        insn_for_pdf["num_days"] = int(i.split(':')[0])
-        insn_for_pdf["date"] = date.strftime("%A, %B %d, %Y")
-        insn_for_pdf["insn_text"] = cat_insns[insn].split(':')[1]
-        insns_for_pdf.append(insn_for_pdf)
+        # insn_for_pdf = {}
+        # date = datetime.date(int(year), int(month), int(day)) - datetime.timedelta(int(i.split(':')[0]))
+        # insn_for_pdf["num_days"] = int(i.split(':')[0])
+        # insn_for_pdf["date"] = date.strftime("%A, %B %d, %Y")
+        # insn_for_pdf["insn_text"] = cat_insns[insn].split(':')[1]
+        # insns_for_pdf.append(insn_for_pdf)
 
-    sorted_insns_for_pdf = sorted(insns_for_pdf, key=lambda insn_for_pdf: insn_for_pdf["num_days"], reverse=True)
+        date = datetime.date(int(year), int(month), int(day)) - datetime.timedelta(int(i.split(':')[0]))
+        num_days = i.split(':')[0]
+        if (num_days in insns_for_pdf):
+            insns_for_pdf[num_days].append(cat_insns[insn].split(':')[1])
+        else:
+            insn_for_pdf = []
+            insn_for_pdf.append(cat_insns[insn].split(':')[1])
+            insns_for_pdf[num_days] = insn_for_pdf
+
+
+    # sorted_insns_for_pdf = sorted(insns_for_pdf, key=lambda insn_for_pdf: insn_for_pdf["num_days"], reverse=True)
     surg_info = [int(month), int(day), int(year)]
-    pdf =  gen_pdf(surg_info, json.dumps(sorted_insns_for_pdf))
+    # pdf =  gen_pdf(surg_info, json.dumps(sorted_insns_for_pdf))
+    pdf =  gen_pdf(surg_info, insns_for_pdf)
     print (pdf)
     return pdf
 
