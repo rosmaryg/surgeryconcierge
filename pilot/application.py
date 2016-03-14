f0369bcd1277ee6bca563b7cec80beef657dd6f6
akatsukac <akatsuka@sas.upenn.edu>
2016-03-14 00:03:37 -0400

    Added data collection entries

--- pilot/application.py
+++ pilot/application.py
@@ -2,25 +2,37 @@ import subprocess
 import requests
 import json
 import datetime
+from twilio.rest import TwilioRestClient
 from flask import request
 from flask import Flask, render_template, make_response
 
 application = Flask(__name__)
+account_sid = "ACed13edfc5830a0afca4ede55a98c58f6"
+auth_token = "a4998472c39156c3b300302bc9e1be38"
+client = TwilioRestClient(account_sid, auth_token)
+
+counter = 0
+
+def send_data(timestamp, medium, success):
+  message = client.messages.create(body="ID: " + str(counter) + "\nRegistered at: " + str(timestamp) + "\nMedium: " + medium + "\nStatus: " + success, to="+14842229088", from_="+12155157414")
 
 @application.route('/')
 def index():
+  global counter
+  counter+=1
   return render_template('index.html')
 
 @application.route('/generate-calendar')
-def genCalendar():
+def genCalendar(): 
   ret_val = subprocess.call(["python", "new_calendar.py", "--data", "\"" + str(request.args) + "\""])
-  if ret_val == 0:
+  if ret_val == 0:	
     return 'Calendar succesfully created!   <a href="http://google.com/calendar">View my calendar</a> <br><br> <button onClick=\"window.close()\"">Close window</button>'
   else:
     return 'Calendar not created.'
 
 @application.route('/generate-ics')
 def genICS():
+  send_data(datetime.datetime.now(), "ICS", "Succeeded")
   print str(request.args)
   ics = subprocess.check_output(["python", "ics.py", "--data", "\"" + str(request.args) + "\""])
   response = make_response(ics)
@@ -30,6 +42,7 @@ def genICS():
 
 @application.route('/generate-pdf')
 def genPdf():
+  send_data(datetime.datetime.now(), "PDF", "Succeeded")
   pdf = subprocess.check_output(["python", "new_pdf.py", "--data", "\"" + str(request.args) + "\""])
   response = make_response(pdf)
   response.headers['Content-Disposition'] = "inline; filename=Instructions.pdf"
@@ -46,8 +59,10 @@ def genText():
   	headers = {'content-type': 'application/json'}
   	r = requests.post(url,data=json.dumps(ret_val),headers=headers)
 	print r.content
+    	send_data(datetime.datetime.now(), "Texts", "Succeeded")
 	return "Texts now being sent."
   except subprocess.CalledProcessError:
+    	send_data(datetime.datetime.now(), "Texts", "Failed")
 	return "Error in sending texts. Double check your entered cell phone number and/or the number you indicated for your pick up person."
 
 
