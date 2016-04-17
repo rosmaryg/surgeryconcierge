3f682a003116020fca826f3d8757aa14390a73d0
akatsukac <akatsuka@sas.upenn.edu>
2016-04-17 08:52:50 -0400

    Integrated joyce's and rosmary's code to make sched stuff

--- schedUI/application.py
+++ schedUI/application.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+import subprocess
+import requests
+import json
+from flask import request
+from flask import Flask, render_template, make_response
 
 application = Flask(__name__)
 
 @application.route('/')
 def index():
   return render_template('index.html')
+
+@application.route('/new-template/', methods=['POST'])
+def newTemplate():
+  print "CREATE"
+  json=request.form['data']
+  ret_val = subprocess.call(["python", "new_template.py", json])
+  if ret_val == 0:
+    return 'New template successfully added.'
+  else:
+    return 'Error: New template not added.'
 
 if __name__ == '__main__':
   application.debug = True
--- schedUI/static/js/custom_script.js
+++ schedUI/static/js/custom_script.js
@@ -1,17 +1,132 @@
-function OnSubmitForm() {
-	var ddb = require('dynamodb').ddb({})
-	console.log(ddb)	
-	var n = document.getElementById("template-name").value;
-	
+AWS.config.update({accessKeyId: 'AKIAJUJB7DVFDSGOBNOQ',
+	    secretAccessKey: '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'});
 
-	if (document.pressed == 'Get PDF') {
-		document.myform.action ="generate-pdf"; 
-	} else if (document.pressed == 'Get ICS') {
-		document.myform.action ="generate-ics";
-	} else if (document.pressed == 'Get Calendar') {
-		document.myform.action ="generate-calendar";
-	} else if(document.pressed == 'Get Texts') {
-		document.myform.action ="generate-text";
+var dynamoDB = new AWS.DynamoDB({endpoint: "https://dynamodb.us-east-1.amazonaws.com", region:"us-east-1"});
+
+
+dynamoDB.listTables({}, function(err, data) {
+	if (err) console.log(err, err.stack);
+	else     console.log(data);
+    });
+
+
+dynamoDB.scan({ TableName: 'surgery-concierge-templates' }, function(err, data) {
+	if (err) console.log(err, err.stack);
+	else {
+	    console.log(data);
+	    var putIn = "<a href='#'><i>No templates found</i></a>";
+	    var menu = document.getElementById("myDropdown");
+	    if (data.Items.length > 0) {
+	        putIn = "";
+		for (var i = 0; i < data.Items.length; i++) {
+		    var info = JSON.parse(data.Items[i].template_name['S']);
+		    putIn += "<a href='javascript:displayContents(" + data.Items[i].template_name['S'] + ")'>" + info['title'] + "</a>";
+		}
+	    }
+	    menu.innerHTML = putIn;
+		
+	}                                                                                                                                                                             
+    });
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+	var dropdowns = document.getElementsByClassName("dropdown-content");
+	var i;
+	for (i = 0; i < dropdowns.length; i++) {
+	    var openDropdown = dropdowns[i];
+	    if (openDropdown.classList.contains('show')) {
+		openDropdown.classList.remove('show');
+    		document.getElementById("sect").innerHTML = event.target.innerHTML; 
+	    }
 	}
-	return true;
+    }
+}
+
+function createNewTemplate() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = "<input type='checkbox'/>";
+    cell2.innerHTML = "<input type='text'/>";
+    cell3.innerHTML = "<input type='text'/>";
+    cell4.innerHTML = "<input type='text'/>";
+    return false;
+}
+
+function onSubmitForm() {
+    var n = document.getElementById("template-name").value;
+    var o = {};
+    o["title"] = n;
+    var idx = 0;
+    var table = document.getElementById("myTable");
+    for(var i = 1; i < table.rows.length; i++) {
+	if (table.rows[i].cells[0].firstChild.checked) {
+	    var toAdd = [{"cond": table.rows[i].cells[1].firstChild.value},
+			 {"insn": table.rows[i].cells[2].firstChild.value},
+			 {"time": table.rows[i].cells[2].firstChild.value}];
+	    o[idx++] = toAdd;
+	}
+    }
+    var key = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for ( var i=0; i < 7; i++ )
+	key += possible.charAt(Math.floor(Math.random() * possible.length));
+    var params = {
+	TableName: 'surgery-concierge-surgeries',
+	Item: {
+	    'template_name' : { "key" : key,
+				"S": JSON.stringify(o) }
+	}
+    }
+    
+    dynamoDB.scan({ TableName: 'surgery-concierge-surgeries' }, function(err, data) {
+	    if (err) console.log(err, err.stack);
+	    else {
+		for (var i = 0; i < data.Items.length; i++) {
+		    var info = JSON.parse(data.Items[i].template_name['S']);
+		    console.log(n);
+		    console.log(info['title']);
+		    if (info['title'] == n) {
+			console.log(n + " is an existing template; please retitle your new template.");
+			return;
+		    }
+		}
+	    }
+	});
+
+    dynamoDB.putItem(params, function(err, data) {
+        if (err) {
+	    console.log("Unable to put item. Error JSON:", JSON.stringify(err, null, 2));
+	    alert("Error adding template. Please try again.");
+	} else {
+	    console.log("PutItem succeeded:", JSON.stringify(data, null, 2));
+	    document.getElementByID("the-form").reset();
+	    alert(n +  " has been added.");
+	}
+    });
+}
+
+function displayContents(info) {
+    var idx = 1;
+    for (var k in info) {
+	if (k == "title") continue;
+	var table = document.getElementById("myTable");
+	var row = table.insertRow(idx++);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
+	cell1.innerHTML = "<input type='checkbox' checked/>";
+	cell2.innerHTML = "<input type='text' value='" + info[k][0].cond + "'/>";
+	cell3.innerHTML = "<input type='text' value='" + info[k][1].insn + "'/>";
+	cell4.innerHTML = "<input type='text' value='" + info[k][2].time + "'/>";
+	console.log(info[k][0].cond);
+    }
 }
