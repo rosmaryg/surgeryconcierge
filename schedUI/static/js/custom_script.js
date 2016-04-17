0c91dadf4787ef9af1335182ff06241116891487
akatsukac <akatsuka@sas.upenn.edu>
2016-04-17 09:00:09 -0400

    Changed up name & removed old methods

--- schedUI/static/js/custom_script.js
+++ schedUI/static/js/custom_script.js
@@ -46,19 +46,6 @@ window.onclick = function(event) {
     }
 }
 
-function createNewTemplate() {
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    cell1.innerHTML = "<input type='checkbox'/>";
-    cell2.innerHTML = "<input type='text'/>";
-    cell3.innerHTML = "<input type='text'/>";
-    cell4.innerHTML = "<input type='text'/>";
-    return false;
-}
 
 function onSubmitForm() {
     var n = document.getElementById("template-name").value;
